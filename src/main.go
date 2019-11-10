205c2804622b746a9b11998fcbfa41db145f26ca
Jon Fairbanks <fairplay89@gmail.com>
2019-11-09 21:20:30 -0800

    Serve from CWD

--- src/main.go
+++ src/main.go
@@ -26,8 +26,18 @@ var (
 )
 
 func main() {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if dir == "/" {
+		// Do nothing
+	} else {	
+		dir = dir + "/public"
+	}
+
 	flag.StringVar(&port, "port", ":8000", "Port to run on (Defaults to :8000)")
-	flag.StringVar(&publicDir, "publicdir", "./src/public", "Directory to serve files from (Defaults to ./src/public)")
+	flag.StringVar(&publicDir, "publicdir", dir, "Directory to serve files from (Defaults to current directory + /public)")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "go-http: ", log.LstdFlags)
